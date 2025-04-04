e2585e174c6629f56784af10888d690b5be90cab
Alp Tahta <alp.tht48@gmail.com>
2025-04-04 12:21:23 +0300

    Receive repo host from user

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+type ServiceConfig struct {
+	RepoHost int
+}
+
 func main() {
-	fmt.Println("test")
+	serviceConfig := ServiceConfig{}
+
+	fmt.Println("Where is your microservice hosted?\n" +
+		"1. Remote repository (GitHub, GitLab, etc.)\n" +
+		"2. Local machine\n" +
+		"Enter choice (1/2):")
+	fmt.Scanln(&serviceConfig.RepoHost)
+	fmt.Println(serviceConfig)
 }
